514ced6da98a6dacc22488327578e04636c17457
Ankur09-ok <ankurborahx7@gmail.com>
2025-09-09 09:51:12 +0530

    Add files via upload

---
+++ Ankur 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// this code is developed by Ankur Borah
+
+int main() {
+	double a, b, c, s, area;
+	
+	cout << "enter the lengths of the three sides of the triangle: ";
+	cin >> a >> b >> c;
+	
+	if (a+b > c && a+c > b && b+c > a) {
+		// semi-perimeter
+		s= (a+b+c) / 2.0
+		
+		// Heron's formula
+		area = sqrt( s * (s-a) * ))
+	}
+}
